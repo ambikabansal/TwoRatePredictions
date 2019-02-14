eecf6575b94b251d2342fb72ee0978fb1ed8cfd9
Ambika Bansal <ambika@eduroam-198-96-33-88.eduroam.yorku.ca>
2019-02-14 12:56:43 -0500

    function to plot perturbation schedule

--- R/Figures.R
+++ R/Figures.R
@@ -1,1 +1,29 @@
+plotRotationSchedule <- function(reversaltrials = 12, reversalmagnitude = -30) {
+  
+  alignedtrials <- 32
+  rotatedtrials <- 100
+  rotatedmagnitude <- 30
+  errorclamptrials <- 20
+  errorclampmagnitude <- 0
+  
+  
+  x <- c(1,
+         alignedtrials +1, 
+         alignedtrials +1,
+         alignedtrials + rotatedtrials +1,
+         alignedtrials + rotatedtrials +1,
+         alignedtrials + rotatedtrials + reversaltrials+ 1,
+         alignedtrials + rotatedtrials + reversaltrials+ 1,
+         alignedtrials + rotatedtrials + reversaltrials + errorclamptrials)
 
+  y <- c(0,
+         0,
+         rotatedmagnitude,
+         rotatedmagnitude,
+         reversalmagnitude,
+         reversalmagnitude,
+         errorclampmagnitude,
+         errorclampmagnitude)
+  
+  lines(x,y,col='black')
+}
